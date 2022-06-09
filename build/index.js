31bc5ef910a0ccd34a669e12ecfc02e60b87c4ec
manishmedp <83647274+manishmedp@users.noreply.github.com>
2022-06-09 09:28:43 +0000

    Initial commit

---
+++ build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const morgan_1 = __importDefault(require("morgan"));
+const server = (0, express_1.default)();
+const PORT = process.env.PORT || 9999;
+server.use(express_1.default.urlencoded({ extended: true }));
+server.use((0, cors_1.default)({ origin: "*" }));
+server.use((0, morgan_1.default)("combined"));
+server.get("/", (req, res) => {
+    res.send("Hello");
+});
+server.listen(PORT, () => console.log("starting on ", PORT));
---
+++ index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const server = express();
+const PORT = process.env.PORT || 9999;
+
+server.use(express.urlencoded({ extended: true }));
+server.use(cors({ origin: "*" }));
+server.use(morgan("combined"));
+
+server.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello");
+});
+
+server.listen(PORT, () => console.log("starting on ", PORT));
